07f634a30faf350b3c7f48f76a1eb0ba62ab9ac1
Pierre Cadart <pierre.cadart@exotec.com>
2024-12-16 13:50:06 +0100

    Finish day 16

--- aoc16/src/main.rs
+++ aoc16/src/main.rs
@@ -1,13 +1,31 @@
 use std::fs;
 use std::env;
-use std::collections::HashSet;
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::collections::VecDeque;
 use std::collections::hash_map::Entry;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+
+    let contents = fs::read_to_string(filename)
+        .expect("Should have been able to read the file");
+
+    let laby = parse_map(&contents);
+    let best_infos = explore_base_paths(&laby);
+
+    let best_path = best_path_from_infos(&laby, &best_infos);
+    let cost = path_cost(&best_path);
+
+    let benches = explored_from_infos(&laby, &best_infos).len();
+
+    println!("Result: {}", cost);
+    println!("Result2: {}", benches);
 }
+
+const COST_FORWARD: usize = 1;
+const COST_TURN: usize = 1000;
 
 #[derive(PartialEq, Eq, Clone, Copy, Debug, std::hash::Hash)]
 enum Cell {
@@ -128,6 +146,7 @@ fn get_moved_position(pos: (usize, usize), size: (usize, usize), dir: Direction)
     }
 }
 
+#[derive(Clone, Copy)]
 enum Action {
     TurnLeft,
     TurnRight,
@@ -135,7 +154,8 @@ enum Action {
 }
 
 struct ExploreInfo {
-    parent: Option<((usize,usize),Direction)>,
+    parents: Vec<((usize,usize),Direction)>,
+    parent_action: Vec<Action>,
     distance: usize
 }
 
@@ -143,28 +163,53 @@ fn insert_sorted_by<T,F>(vec: &mut VecDeque<T>, elem: T, mut sort_func: F)
     where F: FnMut(&T)->usize
 {
     let target_key = sort_func(&elem);
-    let search_res = vec.binary_search_by_key(&target_key, sort_func);
-    let target_index = search_res.unwrap();
-    vec.insert(target_index, elem);
+    let search_res = vec.binary_search_by_key(&target_key, &mut sort_func);
+
+    if let Ok(i) = search_res {
+        vec.insert(i, elem);
+        //let distances: Vec<usize> = vec.iter().map(sort_func).collect();
+        //println!("After insert ok of {} at {}: {:?}", target_key, i, distances);
+    }
+    else if let Err(i) = search_res {
+        vec.insert(i, elem);
+        //let distances: Vec<usize> = vec.iter().map(sort_func).collect();
+        //println!("After insert err of {} at {}: {:?}", target_key, i, distances);
+    }
 }
 
-fn best_path(map: &Map) -> Vec<Action> {
+fn explore_base_paths(map: &Map) -> HashMap<((usize,usize),Direction), ExploreInfo> {
     let mut infos: HashMap<((usize,usize),Direction), ExploreInfo> = HashMap::new();
     let mut to_explore: VecDeque<_> = VecDeque::from( [(map.start, Direction::Right)] );
-    infos.insert((map.start, Direction::Right), ExploreInfo{parent: None, distance:0});
+    infos.insert((map.start, Direction::Right), ExploreInfo{
+        parents: vec![],
+        parent_action: vec![],
+        distance:0
+    });
 
-    const COST_FORWARD: usize = 1;
-    const COST_TURN: usize = 1000;
+    let mut prev_distance = 0;
 
     while !to_explore.is_empty() {
         let current = to_explore.pop_front().unwrap();
+        let mut needs_sort = false;
+        /*
+        if current.0 == map.end {
+            break;
+        }
+        */
         let current_distance = infos.get(&current).unwrap().distance;
+        //println!("Explore at distance {}", current_distance);
+        if prev_distance > current_distance {
+            println!("Explore at distance {} after {}", current_distance, prev_distance);
+            panic!();
+        }
+        prev_distance = current_distance;
         // forward
         let moved_forward = get_moved_position(current.0, map.get_size(), current.1);
         if *map.get_cell(moved_forward.unwrap()).unwrap() == Cell::Empty {
             let entry = infos.entry((moved_forward.unwrap(), current.1));
             let new_info = ExploreInfo{
-                parent: Some(current),
+                parents: vec![current],
+                parent_action: vec![Action::Forward],
                 distance: current_distance + COST_FORWARD,
             };
             match entry {
@@ -175,15 +220,20 @@ fn best_path(map: &Map) -> Vec<Action> {
                 Entry::Occupied(mut o) => {
                     if o.get().distance > new_info.distance {
                         o.insert(new_info);
+                        needs_sort = true;
+                    } else if o.get().distance == new_info.distance {
+                        o.get_mut().parents.push(current);
+                        o.get_mut().parent_action.push(Action::Forward);
                     }
                 }
             };
         }
-        // left
+        // left turn
         {
             let entry = infos.entry((current.0, current.1.turn_left()));
             let new_info = ExploreInfo{
-                parent: Some(current),
+                parents: vec![current],
+                parent_action: vec![Action::TurnLeft],
                 distance: current_distance + COST_TURN,
             };
             match entry {
@@ -194,14 +244,117 @@ fn best_path(map: &Map) -> Vec<Action> {
                 Entry::Occupied(mut o) => {
                     if o.get().distance > new_info.distance {
                         o.insert(new_info);
+                        needs_sort = true;
+                    } else if o.get().distance == new_info.distance {
+                        o.get_mut().parents.push(current);
+                        o.get_mut().parent_action.push(Action::TurnLeft);
                     }
                 }
             };
         }
+        // right turn
+        {
+            let entry = infos.entry((current.0, current.1.turn_right()));
+            let new_info = ExploreInfo{
+                parents: vec![current],
+                parent_action: vec![Action::TurnRight],
+                distance: current_distance + COST_TURN,
+            };
+            match entry {
+                Entry::Vacant(v) => {
+                    v.insert(new_info);
+                    insert_sorted_by(&mut to_explore, (current.0, current.1.turn_right()), |e| infos.get(e).unwrap().distance);
+                },
+                Entry::Occupied(mut o) => {
+                    if o.get().distance > new_info.distance {
+                        o.insert(new_info);
+                        needs_sort = true;
+                    } else if o.get().distance == new_info.distance {
+                        o.get_mut().parents.push(current);
+                        o.get_mut().parent_action.push(Action::TurnRight);
+                    }
+                }
+            };
+        }
+        if needs_sort {
+            to_explore.make_contiguous().sort_by_key(|e| infos.get(e).unwrap().distance);
+        }
     }
 
+    infos
+}
 
+fn best_path_from_infos(map: &Map, infos: &HashMap<((usize,usize),Direction), ExploreInfo>) -> Vec<Action> {
     // build backwards
+    let mut end_vec: Vec<((usize,usize),Direction)> = infos.keys().filter(|e| e.0 == map.end).cloned().collect();
+    end_vec.sort_by_key(|item| infos.get(item).unwrap().distance);
+    let mut reached = end_vec.first().cloned();
     let mut res: Vec<Action> = vec![];
+    while reached.is_some() {
+        let pos = reached.unwrap();
+        let info = infos.get(&pos).unwrap();
+        reached = info.parents.first().cloned();
+        if let Some(act) = info.parent_action.first() {
+            res.push(*act);
+        }
+    }
+    res.reverse();
     res
+}
+
+fn explored_from_infos(map: &Map, infos: &HashMap<((usize,usize),Direction), ExploreInfo>) -> HashSet<(usize,usize)> {
+    let mut back_explored: HashSet<((usize,usize),Direction)> = HashSet::new();
+    let mut wave : VecDeque<((usize,usize), Direction)> = VecDeque::new();
+
+    let mut end_vec: Vec<((usize,usize),Direction)> = infos.keys().filter(|e| e.0 == map.end).cloned().collect();
+    let best_score = end_vec.iter().map(|k| infos.get(k).unwrap().distance).min().unwrap();
+
+    for posdir in infos.keys().filter(|e| e.0 == map.end) {
+        if infos.get(posdir).unwrap().distance == best_score {
+            wave.push_back(*posdir);
+            back_explored.insert(*posdir);
+        }
+    }
+
+    while !wave.is_empty() {
+        let explored = wave.pop_front().unwrap();
+        let ancestors = &infos.get(&explored).unwrap().parents;
+        for ancestor in ancestors {
+            if !back_explored.contains(&ancestor) {
+                back_explored.insert(*ancestor);
+                wave.push_back(*ancestor);
+            }
+        }
+    }
+
+    let mut res = HashSet::new();
+    for (pt,_) in back_explored {
+        res.insert(pt);
+    }
+    res
+}
+
+fn path_cost(actions: &[Action]) -> usize {
+    actions.iter().map(|act| match act {
+        Action::Forward => COST_FORWARD,
+        _ => COST_TURN
+    }).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() {
+        let laby = parse_map("###############\n#.......#....E#\n#.#.###.#.###.#\n#.....#.#...#.#\n#.###.#####.#.#\n#.#.#.......#.#\n#.#.#####.###.#\n#...........#.#\n###.#.#####.#.#\n#...#.....#.#.#\n#.#.#.###.#.#.#\n#.....#...#.#.#\n#.###.#.#.#.#.#\n#S..#.....#...#\n###############\n");
+        let best_infos = explore_base_paths(&laby);
+
+        let best_path = best_path_from_infos(&laby, &best_infos);
+        let cost = path_cost(&best_path);
+        assert_eq!(cost, 7036);
+
+        let bench = explored_from_infos(&laby, &best_infos);
+        assert_eq!(bench.len(), 45);
+    }
 }
