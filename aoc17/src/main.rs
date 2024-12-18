fb9f3c7112c60f65f4897be901777f582aa4f4a7
redstorm45 <pierre.cadart@gmail.com>
2024-12-18 19:11:53 +0100

    Finish day 17

--- aoc17/src/main.rs
+++ aoc17/src/main.rs
@@ -1,9 +1,7 @@
 
 use core::panic;
-use std::collections::VecDeque;
 use std::fs;
 use std::env;
-use itertools::Itertools;
 
 /*
 First program:
@@ -39,9 +37,9 @@ fn main() {
         .expect("Should have been able to read the file");
 
     let mut splitter = contents.split_terminator('\n');
-    let reg_a: i32 = splitter.next().unwrap().split(": ").nth(1).unwrap().parse().unwrap();
-    let reg_b: i32 = splitter.next().unwrap().split(": ").nth(1).unwrap().parse().unwrap();
-    let reg_c: i32 = splitter.next().unwrap().split(": ").nth(1).unwrap().parse().unwrap();
+    let reg_a: MachineUnit = splitter.next().unwrap().split(": ").nth(1).unwrap().parse().unwrap();
+    let reg_b: MachineUnit = splitter.next().unwrap().split(": ").nth(1).unwrap().parse().unwrap();
+    let reg_c: MachineUnit = splitter.next().unwrap().split(": ").nth(1).unwrap().parse().unwrap();
     let instructions: Vec<i8> = splitter.nth(1).unwrap().split(": ").nth(1).unwrap().split(',').map(|e| e.parse::<i8>().unwrap()).collect();
 
     let mut state = MachineState{
@@ -55,9 +53,11 @@ fn main() {
 
     let output = run_until_halt(&mut state, &instructions);
 
-    let output_str = output.iter().map(|v| v.to_string()).intersperse(String::from(",")).fold(String::new(), |cur, nxt| cur + &nxt);
+    let iter1 = output.iter().map(|v| v.to_string());
+    let output_str = itertools::intersperse(iter1, String::from(",")).fold(String::new(), |cur, nxt| cur + &nxt);
     println!("Result: {}", output_str);
 
+    /*
     let mut found = false;
     for i in 5..1000000000 {
         let mut state2 = MachineState{
@@ -75,20 +75,39 @@ fn main() {
     if !found {
         println!("Machine not found");
     }
+    */
+
+    let rev = revert_program(&instructions);
+    println!("Result2: {}", rev);
+
+    /*
+    let mut state = MachineState{
+        register_a: 202322936867370,
+        register_b: 0,
+        register_c: 0,
+        program_counter: 0,
+    };
+
+    let output2 = run_until_halt(&mut state, &instructions);
+    let output2_str = output2.iter().map(|v| v.to_string()).intersperse(String::from(",")).fold(String::new(), |cur, nxt| cur + &nxt);
+    println!("New result: {}", output2_str);
+    */
 }
+
+type MachineUnit = usize;
 
 #[derive(Debug)]
 struct MachineState {
-    register_a: i32,
-    register_b: i32,
-    register_c: i32,
+    register_a: MachineUnit,
+    register_b: MachineUnit,
+    register_c: MachineUnit,
     program_counter: usize
 }
 
-fn read_combo(state: &mut MachineState, instructions: &[i8], index: usize) -> i32 {
+fn read_combo(state: &mut MachineState, instructions: &[i8], index: usize) -> MachineUnit {
     let target = instructions[index];
     match target {
-        0..=3 => target as i32,
+        0..=3 => target as MachineUnit,
         4 => state.register_a,
         5 => state.register_b,
         6 => state.register_c,
@@ -110,7 +129,7 @@ fn run_one_step(state: &mut MachineState, instructions: &[i8]) -> (Option<i8>,bo
         },
         1 => { // bxl
             let operand = instructions[state.program_counter+1];
-            state.register_b = state.register_b ^ (operand as i32);
+            state.register_b = state.register_b ^ (operand as MachineUnit);
             state.program_counter += 2;
         },
         2 => { // bst
@@ -168,165 +187,33 @@ fn run_until_halt(state: &mut MachineState, instructions: &[i8]) -> Vec<i8> {
     res
 }
 
-#[derive(PartialEq, Eq)]
-enum Register{
-    A,
-    B,
-    C
-}
+fn revert_program(target_output: &[i8]) -> usize {
+    // hardcoded algorithm for this program
+    // every step formula is:
+    // a_ ^ 5 ^ ((a>>(a_^1)) %8) = out
+    // with a the full reg_a value, and a_ the lowest 3 bits
+    // before first loop, a=0
+    // before formula, a<<3 -> every step, try all a_
 
-// possible state of the program at an intermediate backtracking step
-// register is None if any value works
-struct BackTrackState {
-    program_counter: usize, // program counter after executing previous instruction
-    completed_output: usize, // number of outputs done up to this point (counting from end)
-    register_a: Option<i32>,
-    register_b: Option<i32>,
-    register_c: Option<i32>,
-    completed: bool, // start of the program has been reached
-}
+    let mut values: Vec<usize> = vec![0];
 
-impl BackTrackState {
-    fn with_program_counter_diff(&self, pc_diff: isize) -> BackTrackState {
-        self.with_program_counter(((self.program_counter as isize)+pc_diff) as usize)
-    }
-    fn with_program_counter(&self, pc: usize) -> BackTrackState {
-        BackTrackState{
-            program_counter: pc,
-            completed_output: self.completed_output,
-            register_a: self.register_a,
-            register_b: self.register_b,
-            register_c: self.register_c,
-            completed: self.completed
-        }
-    }
-    fn with_completed(&self) -> BackTrackState {
-        BackTrackState{
-            program_counter: self.program_counter,
-            completed_output: self.completed_output,
-            register_a: self.register_a,
-            register_b: self.register_b,
-            register_c: self.register_c,
-            completed: true
-        }
-    }
-    fn with_register_a(&self, reg_a: Option<i32>) -> BackTrackState {
-        BackTrackState{
-            program_counter: self.program_counter,
-            completed_output: self.completed_output,
-            register_a: reg_a,
-            register_b: self.register_b,
-            register_c: self.register_c,
-            completed: self.completed
-        }
-    }
-    fn with_register_b(&self, reg_b: Option<i32>) -> BackTrackState {
-        BackTrackState{
-            program_counter: self.program_counter,
-            completed_output: self.completed_output,
-            register_a: self.register_a,
-            register_b: reg_b,
-            register_c: self.register_c,
-            completed: self.completed
-        }
-    }
-    fn with_register_c(&self, reg_c: Option<i32>) -> BackTrackState {
-        BackTrackState{
-            program_counter: self.program_counter,
-            completed_output: self.completed_output,
-            register_a: self.register_a,
-            register_b: self.register_b,
-            register_c: reg_c,
-            completed: self.completed
-        }
-    }
-    fn with_register(&self, reg: Register, value: Option<i32>) -> BackTrackState {
-        match reg {
-            Register::A => self.with_register_a(value),
-            Register::B => self.with_register_b(value),
-            Register::C => self.with_register_c(value),
-        }
-    }
-    fn get_register(&self, reg: Register) -> Option<i32> {
-        match reg {
-            Register::A => self.register_a,
-            Register::B => self.register_b,
-            Register::C => self.register_c
-        }
-    }
-}
-
-fn propose_shifting_backtracks(state: &BackTrackState, current_instruction: i8, current_arg: i8) -> Vec<BackTrackState> {
-    // group adv, bdc, cdv into one call
-    // reverses instructions (a/b/c) = a>>(a/b/c/0..3)
-    let target_register = match current_instruction {
-        0 => Register::A,
-        6 => Register::B,
-        7 => Register::C,
-        _ => panic!("Unsupported division")
-    };
-    match current_arg {
-        0..=3 => {
-            // fixed shift => remainder has been lost
-            if let Some(reg) = state.get_register(target_register) {
-                let mut res = vec![];
-                for remainder in 0..=(2<<current_arg) {
-                    res.push(
-                        state.with_program_counter_diff(-2)
-                             .with_register_a(Some((reg<<current_arg) + remainder))
-                    );
+    for output in target_output.iter().rev() {
+        //println!("Revert from value {:?} with output {}", values, output);
+        let mut new_values = vec![];
+        for v in values {
+            for i in 0..=7 {
+                let a = v*8 +i;
+                if i ^ 5 ^ ((a>>(i^1))%8) == *output as usize {
+                    new_values.push(a);
                 }
-                res
-            } else {
-                vec![state.with_program_counter_diff(-2)]
             }
-        },
-        4..=6 => {
-            // variable shift, 
         }
-        _ => {panic!("Unsupported division");}
-    }
-}
-
-fn propose_backtracks(state: &BackTrackState, next_output: Option<i8>, program: &[i8]) -> Vec<BackTrackState> {
-    // propose backtracking options for one state
-    let mut possibilities = vec![];
-    // TODO: backtracking of jmp here
-    // suppose there was no jmp
-
-    if state.program_counter == 0 {
-        possibilities.push(state.with_completed());
-    } else {
-        let prev_instruction = program[state.program_counter-2];
-        let prev_arg = program[state.program_counter-1];
-
-        match prev_instruction {
-            0 => { // adv   a>>arg -> a
-                possibilities.extend(propose_shifting_backtracks(state, prev_instruction, prev_arg));
-            },
-            _ => panic!("Unsupported instruction")
-        }
+        values = new_values;
     }
 
-    possibilities
-}
+    values.sort();
 
-fn backtrack_all(initial: BackTrackState, target_output: &[i8], program: &[i8]) -> Vec<BackTrackState> {
-    // backtrack all paths until start of the program
-    // does a breadth-first traversal until all roots (backtrack at start with complete output) are found
-    // and returns all those roots
-
-    // TODO: parse jmp table first, [targetaddress -> jmp address]
-
-    let mut to_explore : VecDeque<BackTrackState> = VecDeque::from([initial]);
-    let mut roots: Vec<BackTrackState> = vec![];
-
-    while to_explore.len() > 0 {
-        let exploredState = to_explore.pop_front().unwrap();
-
-    }
-
-    roots
+    *values.first().unwrap()
 }
 
 #[cfg(test)]
